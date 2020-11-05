8fe3541ad8f1d4507f1ced4756482b4f4037bd4c
ndossett <niki.m.dossett@gmail.com>
2020-11-05 01:25:38 -0500

    started likeSection

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ const App = () => {
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       <SearchBar />
-      <Posts posts={setPosts} />
+      <Posts posts={posts} likePostFunc={likePost} />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
